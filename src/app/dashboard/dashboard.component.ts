e45bd064f709478972afe97b5dae6fa509f76e0a
Mauricio Martinez <mauroz77@gmail.com>
2020-12-04 16:38:32 +0000

    Fix Sonar Bugs
    - Fix several bugs y vulnerabilities reported by SonarQube
    - From 104 bugs to 9 and from 10 vulnerabilities/Security Hotspots from 10 to 0.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-declare function pdxFinderbarChart(
-    title: string,
-    data: any,
-    cssID: string,
-    categoryField: string,
-    valueField: string,
-    labelRotation: number): any;
-
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
@@ -17,28 +9,6 @@ export class DashboardComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-        const chartData = [{
-            mapping: 'JAX',
-            visits: 3
-        }, {
-            mapping: 'IRCC',
-            visits: 7
-        }, {
-            mapping: 'PDMR',
-            visits: 5
-        }, {
-            mapping: 'PDXNET-HCI-BCM',
-            visits: 0
-        }, {
-            mapping: 'PDXNET-MDANDERSON',
-            visits: 2
-        }, {
-            mapping: 'PDXNET-MDANDERSON-PENN ',
-            visits: 0
-        }, {
-            mapping: 'PDXNET-WUSTL',
-            visits: 0
-        }];
 
     }
 
