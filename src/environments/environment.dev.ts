d4423779cd1feeb5a390b0b8b9695c3895c5c7c7
Mauricio Martinez <mauroz77@gmail.com>
2021-03-10 09:53:22 +0000

    Allow to have dev and prod environments with different apiServer urls

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiServer: 'http://hh-rke-wp-webadmin-20-worker-1.caas.ebi.ac.uk:32368/'
+  };
+  
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiServer: 'http://hh-rke-wp-webadmin-20-worker-1.caas.ebi.ac.uk:32368/'
+  apiServer: 'http://hh-rke-wp-webadmin-20-worker-1.caas.ebi.ac.uk:31847/'
 };
